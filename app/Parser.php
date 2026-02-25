b83072065c4bba7f248679ecee50ed901055a3ff
Stephen Heron <stephen@learntolovedigital.com>
2026-02-25 22:03:37 +0000

    Update leaderboard: optimize CSV parser (wall time 1.77s)

--- app/Parser.php
+++ app/Parser.php
@@ -8,6 +8,46 @@ final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $input = fopen($inputPath, 'rb');
+
+        if ($input === false) {
+            throw new Exception("Unable to open input file: {$inputPath}");
+        }
+
+        stream_set_read_buffer($input, 1024 * 1024);
+
+        $visits = [];
+        $hostPrefixLength = 19; // https://stitcher.io
+
+        while (($line = fgets($input)) !== false) {
+            $commaPosition = strpos($line, ',');
+            $path = substr($line, $hostPrefixLength, $commaPosition - $hostPrefixLength);
+            $date = substr($line, $commaPosition + 1, 10);
+
+            if (isset($visits[$path][$date])) {
+                ++$visits[$path][$date];
+                continue;
+            }
+
+            $visits[$path][$date] = 1;
+        }
+
+        fclose($input);
+
+        foreach ($visits as &$dates) {
+            ksort($dates);
+        }
+
+        unset($dates);
+
+        $encoded = json_encode($visits, JSON_PRETTY_PRINT);
+
+        if ($encoded === false) {
+            throw new Exception('Unable to encode JSON output');
+        }
+
+        if (file_put_contents($outputPath, $encoded) === false) {
+            throw new Exception("Unable to write output file: {$outputPath}");
+        }
     }
 }
